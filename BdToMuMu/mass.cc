6c3dd51365f7ca844f7c00b4eaf0042de7458928
luca <luca.martini82@gmail.com>
2014-09-05 17:03:21 +0200

    mass resolution vs y

--- BdToMuMu/mass.cc
+++ BdToMuMu/mass.cc
@@ -1,6 +1,7 @@
 #include "TFile.h"
 #include "TTree.h"
 #include "TH1D.h"
+#include "TH2D.h"
 #include "TClonesArray.h"
 #include "TCanvas.h"
 #include "TLorentzVector.h"
@@ -10,6 +11,8 @@
 #include "TLatex.h"
 #include "TF1.h"
 #include "TFitResult.h"
+#include "TLegend.h"
+#include "TStyle.h"
 
 #include <iostream>
 #include <sstream>
@@ -44,7 +47,7 @@ string insert(string string_i, double value, string readFile_ = "effSLHC.csv") {
   return out;
 }
 
-vector <double> plot(TH1D * h_5314, TH1D * h_620SLHC7, string label, bool fit = false, bool stack = false, particle p = muon) {
+vector <double> plot(TH1D * h_5314, TH1D * h_620SLHC7, string label, bool fit = false, bool stack = false, particle p = muon, bool latex = true) {
 
   vector <double> sigmas;
 
@@ -64,7 +67,7 @@ vector <double> plot(TH1D * h_5314, TH1D * h_620SLHC7, string label, bool fit = 
 
 	TCanvas * c = new TCanvas("c", "c", 600, 600);
 	h_5314->SetStats(0);
-	h_5314->SetMaximum(1.1*max(h_5314->GetMaximum(), h_620SLHC7->GetMaximum()));
+	h_5314->SetMaximum(1.1*max(h_5314->GetBinContent(h_5314->GetMaximumBin()), h_620SLHC7->GetBinContent(h_620SLHC7->GetMaximumBin())));
 	h_5314->GetYaxis()->SetTitleOffset(1.4);
 	h_5314->SetLineColor(kBlue);
 	h_5314->SetFillColor(kBlue);
@@ -92,32 +95,36 @@ vector <double> plot(TH1D * h_5314, TH1D * h_620SLHC7, string label, bool fit = 
 	}
 	h_620SLHC7->Draw("same");
 
-	TLatex * st_5314 = new TLatex();
-	st_5314->SetTextSize(0.023);
-	st_5314->SetTextColor(kBlue);
-	st_5314->DrawLatexNDC(0.6, 0.8, h_5314->GetName());
-	st_5314->DrawLatexNDC(0.6, 0.76, Form("mean = %f", h_5314->GetMean()));
-	st_5314->DrawLatexNDC(0.6, 0.72, Form("RMS = %f #pm %f", h_5314->GetRMS(), h_5314->GetRMSError()));
-	if (fit) {
-	  if (p == muon || 1) {
-	   st_5314->DrawLatexNDC(0.6, 0.68, Form("#mu = %f #pm %f", r_5314->Parameter(1), r_5314->ParError(1)));
-	   st_5314->DrawLatexNDC(0.6, 0.64, Form("#sigma = %f #pm %f", r_5314->Parameter(2), r_5314->ParError(2)));
+  if (latex) {
+	  TLatex * st_5314 = new TLatex();
+	  st_5314->SetTextSize(0.023);
+	  st_5314->SetTextColor(kBlue);
+	  st_5314->DrawLatexNDC(0.6, 0.8, h_5314->GetName());
+	  st_5314->DrawLatexNDC(0.6, 0.76, Form("mean = %f", h_5314->GetMean()));
+	  st_5314->DrawLatexNDC(0.6, 0.72, Form("RMS = %f #pm %f", h_5314->GetRMS(), h_5314->GetRMSError()));
+	  if (fit) {
+	    if (p == muon || 1) {
+	      st_5314->DrawLatexNDC(0.6, 0.68, Form("#mu = %f #pm %f", r_5314->Parameter(1), r_5314->ParError(1)));
+	      st_5314->DrawLatexNDC(0.6, 0.64, Form("#sigma = %f #pm %f", r_5314->Parameter(2), r_5314->ParError(2)));
+	    }
+    }
+  }
+  if (fit) sigmas.push_back(r_5314->Parameter(2));	
+  if (latex) {
+	  TLatex * st_620SLHC7 = new TLatex();
+	  st_620SLHC7->SetTextSize(0.023);
+	  st_620SLHC7->SetTextColor(kRed);
+	  st_620SLHC7->DrawLatexNDC(0.6, 0.6, h_620SLHC7->GetName());
+	  st_620SLHC7->DrawLatexNDC(0.6, 0.56, Form("mean = %f", h_620SLHC7->GetMean()));
+	  st_620SLHC7->DrawLatexNDC(0.6, 0.52, Form("RMS = %f #pm %f", h_620SLHC7->GetRMS(), h_620SLHC7->GetRMSError()));
+	  if (fit) {
+	    if (p == muon || 1) {
+	      st_620SLHC7->DrawLatexNDC(0.6, 0.48, Form("#mu = %f #pm %f", r_620SLHC7->Parameter(1), r_620SLHC7->ParError(1)));
+	      st_620SLHC7->DrawLatexNDC(0.6, 0.44, Form("#sigma = %f #pm %f", r_620SLHC7->Parameter(2), r_620SLHC7->ParError(2)));
+	    }
 	  }
-	   sigmas.push_back(r_5314->Parameter(2));
-	}
-	TLatex * st_620SLHC7 = new TLatex();
-	st_620SLHC7->SetTextSize(0.023);
-	st_620SLHC7->SetTextColor(kRed);
-	st_620SLHC7->DrawLatexNDC(0.6, 0.6, h_620SLHC7->GetName());
-	st_620SLHC7->DrawLatexNDC(0.6, 0.56, Form("mean = %f", h_620SLHC7->GetMean()));
-	st_620SLHC7->DrawLatexNDC(0.6, 0.52, Form("RMS = %f #pm %f", h_620SLHC7->GetRMS(), h_620SLHC7->GetRMSError()));
-	if (fit) {
-	  if (p == muon || 1) {
-	   st_620SLHC7->DrawLatexNDC(0.6, 0.48, Form("#mu = %f #pm %f", r_620SLHC7->Parameter(1), r_620SLHC7->ParError(1)));
-	   st_620SLHC7->DrawLatexNDC(0.6, 0.44, Form("#sigma = %f #pm %f", r_620SLHC7->Parameter(2), r_620SLHC7->ParError(2)));
-	  }
-	   sigmas.push_back(r_620SLHC7->Parameter(2));
-	}
+  }
+  if (fit) sigmas.push_back(r_620SLHC7->Parameter(2));
 
 	string name(h_5314->GetName());
 	std::size_t found = name.find("_h_");
@@ -138,12 +145,96 @@ vector <double> plot(TH1D * h_5314, TH1D * h_620SLHC7, string label, bool fit = 
 	return sigmas;
 }
 
+void plot2D(TH2D * h) {
+  TCanvas * c = new TCanvas("c", "c", 600, 600);
+  h->SetStats(false);
+  h->Draw("COLZ");
+  c->Print(Form("%s%s.pdf", tight? "tight/" : "./", h->GetName()));
+  delete c;
+}
+
+void plot_res(TH1D * h_5314, TH1D * h_620SLHC7, string label) {
+  TCanvas c("c", "c", 600, 600);
+
+  TPad pad1("pad1", "", 0, 0.33, 1, 1, 0, 0, 0);
+  TPad pad2("pad2", "", 0, 0, 1, 0.33, 0, 0, 0);
+  pad1.SetBottomMargin(0.001);
+  pad2.SetTopMargin(0.005);
+  pad2.SetBottomMargin(pad2.GetBottomMargin()*3);
+  pad1.Draw();
+  pad2.Draw();
+
+  pad1.SetGrid();
+  pad2.SetGrid();
+
+  double labelSizeFactor1 = (pad1.GetHNDC()+pad2.GetHNDC()) / pad1.GetHNDC();
+  double labelSizeFactor2 = (pad1.GetHNDC()+pad2.GetHNDC()) / pad2.GetHNDC();
+
+  pad1.cd();
+
+  h_5314->SetStats(0);
+  h_5314->SetMaximum(1.1*max(h_5314->GetBinContent(h_5314->GetMaximumBin()), h_620SLHC7->GetBinContent(h_620SLHC7->GetMaximumBin())));
+  h_5314->SetMinimum(.9*min(h_5314->GetBinContent(h_5314->GetMinimumBin()), h_620SLHC7->GetBinContent(h_620SLHC7->GetMinimumBin())));
+  h_5314->GetYaxis()->SetTitleOffset(1.4);
+  h_5314->SetLineColor(kBlue);
+  h_5314->SetMarkerStyle(20);
+  h_5314->SetMarkerColor(kBlue);
+  h_5314->SetMarkerSize(1);
+  h_5314->SetYTitle("#sigma^{#mu#mu}(GeV)");
+  h_5314->SetTitle("Mass width vs rapidity");
+  h_5314->SetLabelSize(gStyle->GetLabelSize()*labelSizeFactor1, "XYZ");
+  h_5314->SetTitleSize(gStyle->GetTitleSize()*labelSizeFactor1, "XYZ");
+  h_5314->GetYaxis()->SetTitleOffset(h_5314->GetYaxis()->GetTitleOffset()/labelSizeFactor1);
+  h_5314->Draw();
+
+  h_620SLHC7->SetStats(0);
+  h_620SLHC7->SetLineColor(kRed);
+  h_620SLHC7->SetMarkerStyle(21);
+  h_620SLHC7->SetMarkerColor(kRed);
+  h_620SLHC7->SetMarkerSize(1);
+  h_620SLHC7->Draw("same");
+
+  TLegend * leg = new TLegend(0.15,0.7,0.48,0.85);
+  //leg->SetHeader("The Legend Title");
+  leg->AddEntry(h_5314, h_5314->GetName(), "lep");
+  leg->AddEntry(h_620SLHC7, h_620SLHC7->GetName(), "lep");
+  leg->Draw();
+
+  pad2.cd();
+
+  h_5314->Sumw2();
+  h_620SLHC7->Sumw2();
+  TH1D * h_ratio = (TH1D*)h_620SLHC7->Clone("ratio 2023 / 2019age1k");
+  h_ratio->Divide(h_5314);
+  h_ratio->GetYaxis()->SetTitleOffset(1.4);
+  h_ratio->SetYTitle("ratio");
+  h_ratio->SetLabelSize(gStyle->GetLabelSize()*labelSizeFactor2*0.8, "XYZ");
+  h_ratio->SetTitleSize(gStyle->GetTitleSize()*labelSizeFactor2*0.8, "XYZ");
+  h_ratio->GetYaxis()->SetTitleOffset(h_ratio->GetYaxis()->GetTitleOffset()/labelSizeFactor2);
+  h_ratio->SetTitle("");
+  h_ratio->SetMarkerStyle(8);
+  h_ratio->SetMarkerColor(kBlack);
+  h_ratio->SetMarkerSize(1);
+  h_ratio->SetLineColor(kBlack);
+  h_ratio->SetMaximum(1.1*h_ratio->GetBinContent(h_ratio->GetMaximumBin()));
+  h_ratio->SetMinimum(.9*h_ratio->GetBinContent(h_ratio->GetMinimumBin()));
+  h_ratio->Draw("e");
+
+  string name(h_5314->GetName());
+  std::size_t found = name.find("_h_");
+  name.erase(found+2, std::string::npos);
+  c.Print(Form("%s%s_%s.pdf", tight? "tight/" : "./", name.c_str(), label.c_str()));
+
+}
+
 void fill(string tail, int mom_pdgId) {
   TFile* input = new TFile(Form("muontree_%s.root", tail.c_str()));
   TTree* tree = (TTree*)input->Get("tree");
 
   TH1D * mass_barrel_h = new TH1D(Form("mass_barrel_h_%s", tail.c_str()), Form("barrel mass (|#eta_{#mu^{1}}| < %.1f AND |#eta_{#mu^{2}}| < %.1f);mass (GeV); Normalized", eta, eta), 100, 4.9, 5.9);
   TH1D * mass_endcap_h = new TH1D(Form("mass_endcap_h_%s", tail.c_str()), Form("endcap mass (|#eta_{#mu^{1}}| > %.1f OR |#eta_{#mu^{2}}| > %.1f);mass (GeV); Normalized", eta, eta), 100, 4.9, 5.9);
+  TH2D * y_mass_h = new TH2D(Form("y_mass_h_%s", tail.c_str()), "Mass vs |y|;|y^{#mu#mu}|;m^{#mu#mu}", 15, 0., 2.4, 100, 4.9, 5.9);
+
   TH1D * pt_res_barrel_h = new TH1D(Form("pt_res_barrel_h_%s", tail.c_str()), "barrel p_{T} resolution (|#eta_{#mu}| < 1.2);p_{T}^{gen}-p_{T}^{reco} (GeV); Normalized", 100, -1., 1.);
   TH1D * pt_res_intermediate_h = new TH1D(Form("pt_res_intermediate_h_%s", tail.c_str()), "intermediate p_{T} resolution (1.2 < |#eta_{#mu}| < 1.6);p_{T}^{gen}-p_{T}^{reco} (GeV); Normalized", 100, -1., 1.);
   TH1D * pt_res_endcap_h = new TH1D(Form("pt_res_endcap_h_%s", tail.c_str()), "endcap p_{T} resolution (1.6 < |#eta_{#mu}| < 2.4);p_{T}^{gen}-p_{T}^{reco} (GeV); Normalized", 100, -1., 1.);
@@ -236,6 +327,7 @@ void fill(string tail, int mom_pdgId) {
                           TLorentzVector dimuon(0.,0.,0.,0.);
                           dimuon = *mu_j + *mu_k;
                           if (dimuon.Pt()>5) {
+                              y_mass_h->Fill(fabs(dimuon.Rapidity()), dimuon.M());
                               if ((abs(mu_j->Eta()) < eta && abs(mu_k->Eta()) < eta)) {
                                   mass_barrel_h->Fill(dimuon.M());
                               }
@@ -249,16 +341,22 @@ void fill(string tail, int mom_pdgId) {
           }
       }
   }
-  if (mass_barrel_h->Integral() < 0.999 || mass_barrel_h->Integral() > 1.001) mass_barrel_h->Scale(1./mass_barrel_h->GetEntries());
-  if (mass_endcap_h->Integral() < 0.999 || mass_endcap_h->Integral() > 1.001) mass_endcap_h->Scale(1./mass_endcap_h->GetEntries());
+  mass_barrel_h->Scale(1./mass_barrel_h->Integral());
+  mass_endcap_h->Scale(1./mass_endcap_h->Integral());
 
-  if (pt_res_barrel_h->Integral() < 0.999 || pt_res_barrel_h->Integral() > 1.001) pt_res_barrel_h->Scale(1./pt_res_barrel_h->GetEntries());
-  if (pt_res_intermediate_h->Integral() < 0.999 || pt_res_intermediate_h->Integral() > 1.001) pt_res_intermediate_h->Scale(1./pt_res_intermediate_h->GetEntries());
-  if (pt_res_endcap_h->Integral() < 0.999 || pt_res_endcap_h->Integral() > 1.001) pt_res_endcap_h->Scale(1./pt_res_endcap_h->GetEntries());
+  TObjArray aSlices;
+  y_mass_h->FitSlicesY(0, 0, -1, 0, "QNR", &aSlices);
+  TH1* slice = (TH1*)aSlices[2];
+
+  pt_res_barrel_h->Scale(1./pt_res_barrel_h->Integral());
+  pt_res_intermediate_h->Scale(1./pt_res_intermediate_h->Integral());
+  pt_res_endcap_h->Scale(1./pt_res_endcap_h->Integral());
 
   TFile * output = new TFile(Form("h_%s.root", tail.c_str()), "recreate");
   mass_barrel_h->Write();
   mass_endcap_h->Write();
+  y_mass_h->Write();
+  slice->Write();
   pt_res_barrel_h->Write();
   pt_res_intermediate_h->Write();
   pt_res_endcap_h->Write();
@@ -275,9 +373,9 @@ int main(int argc, char* argv[]) {
 		}
 	}
 
-    ofstream outfile;
-    outfile.open("effSLHC2.csv");
-    outfile << "Type,old,new\n";
+  ofstream outfile;
+  outfile.open("effSLHC2.csv");
+  outfile << "Type,old,new\n";
 
 	ofstream sigmafile;
 	sigmafile.open("effsigmaSLHC2.csv");
@@ -301,6 +399,8 @@ int main(int argc, char* argv[]) {
 	  TFile * in_2019 = new TFile(Form("h_%s.root", s2019_c.c_str()));
 	  TH1D * mass_barrel_h_2019 = (TH1D*)in_2019->Get(Form("mass_barrel_h_%s", s2019_c.c_str()));
 	  TH1D * mass_endcap_h_2019 = (TH1D*)in_2019->Get(Form("mass_endcap_h_%s", s2019_c.c_str()));
+    TH2D * y_mass_h_2019 = (TH2D*)in_2019->Get(Form("y_mass_h_%s", s2019_c.c_str()));
+    TH1D * y_mass_h_2019_2 = (TH1D*)in_2019->Get(Form("y_mass_h_%s_2", s2019_c.c_str()));
 	  TH1D * pt_res_barrel_h_2019 = (TH1D*)in_2019->Get(Form("pt_res_barrel_h_%s", s2019_c.c_str()));
 	  TH1D * pt_res_intermediate_h_2019 = (TH1D*)in_2019->Get(Form("pt_res_intermediate_h_%s", s2019_c.c_str()));
 	  TH1D * pt_res_endcap_h_2019 = (TH1D*)in_2019->Get(Form("pt_res_endcap_h_%s", s2019_c.c_str()));
@@ -308,6 +408,8 @@ int main(int argc, char* argv[]) {
 	  TFile * in_2019age1k = new TFile(Form("h_%s.root", s2019age1k_c.c_str()));
 	  TH1D * mass_barrel_h_2019age1k = (TH1D*)in_2019age1k->Get(Form("mass_barrel_h_%s", s2019age1k_c.c_str()));
 	  TH1D * mass_endcap_h_2019age1k = (TH1D*)in_2019age1k->Get(Form("mass_endcap_h_%s", s2019age1k_c.c_str()));
+    TH2D * y_mass_h_2019age1k = (TH2D*)in_2019age1k->Get(Form("y_mass_h_%s", s2019age1k_c.c_str()));
+    TH1D * y_mass_h_2019age1k_2 = (TH1D*)in_2019age1k->Get(Form("y_mass_h_%s_2", s2019age1k_c.c_str()));
 	  TH1D * pt_res_barrel_h_2019age1k = (TH1D*)in_2019age1k->Get(Form("pt_res_barrel_h_%s", s2019age1k_c.c_str()));
 	  TH1D * pt_res_intermediate_h_2019age1k = (TH1D*)in_2019age1k->Get(Form("pt_res_intermediate_h_%s", s2019age1k_c.c_str()));
 	  TH1D * pt_res_endcap_h_2019age1k = (TH1D*)in_2019age1k->Get(Form("pt_res_endcap_h_%s", s2019age1k_c.c_str()));
@@ -315,6 +417,8 @@ int main(int argc, char* argv[]) {
 	  TFile * in_2023 = new TFile(Form("h_%s.root", s2023_c.c_str()));
 	  TH1D * mass_barrel_h_2023 = (TH1D*)in_2023->Get(Form("mass_barrel_h_%s", s2023_c.c_str()));
 	  TH1D * mass_endcap_h_2023 = (TH1D*)in_2023->Get(Form("mass_endcap_h_%s", s2023_c.c_str()));
+    TH2D * y_mass_h_2023 = (TH2D*)in_2023->Get(Form("y_mass_h_%s", s2023_c.c_str()));
+    TH1D * y_mass_h_2023_2 = (TH1D*)in_2023->Get(Form("y_mass_h_%s_2", s2023_c.c_str()));
 	  TH1D * pt_res_barrel_h_2023 = (TH1D*)in_2023->Get(Form("pt_res_barrel_h_%s", s2023_c.c_str()));
 	  TH1D * pt_res_intermediate_h_2023 = (TH1D*)in_2023->Get(Form("pt_res_intermediate_h_%s", s2023_c.c_str()));
 	  TH1D * pt_res_endcap_h_2023 = (TH1D*)in_2023->Get(Form("pt_res_endcap_h_%s", s2023_c.c_str()));
@@ -349,6 +453,7 @@ int main(int argc, char* argv[]) {
 	  plot(pt_res_barrel_h_2019, pt_res_barrel_h_2023, label, true);
 	  plot(pt_res_intermediate_h_2019, pt_res_intermediate_h_2023, label, true);
 	  plot(pt_res_endcap_h_2019, pt_res_endcap_h_2023, label, true);
+    plot_res(y_mass_h_2019_2, y_mass_h_2023_2, label);
 
 	  label = Form("2019age1k-2019_%d", pdgId_mom);
 	  plot(mass_barrel_h_2019age1k, mass_barrel_h_2019, label);
@@ -356,6 +461,20 @@ int main(int argc, char* argv[]) {
 	  plot(pt_res_barrel_h_2019age1k, pt_res_barrel_h_2019, label, true);
 	  plot(pt_res_intermediate_h_2019age1k, pt_res_intermediate_h_2019, label, true);
 	  plot(pt_res_endcap_h_2019age1k, pt_res_endcap_h_2019, label, true);
+    plot_res(y_mass_h_2019age1k_2, y_mass_h_2019_2, label);
+
+
+    label = Form("2019age1k-2023_%d", pdgId_mom);
+    plot(mass_barrel_h_2019age1k, mass_barrel_h_2023, label);
+    plot(mass_endcap_h_2019age1k, mass_endcap_h_2023, label);
+    plot(pt_res_barrel_h_2019age1k, pt_res_barrel_h_2023, label, true);
+    plot(pt_res_intermediate_h_2019age1k, pt_res_intermediate_h_2023, label, true);
+    plot(pt_res_endcap_h_2019age1k, pt_res_endcap_h_2023, label, true);
+    plot_res(y_mass_h_2019age1k_2, y_mass_h_2023_2, label);
+
+    plot2D(y_mass_h_2019);
+    plot2D(y_mass_h_2019age1k);
+    plot2D(y_mass_h_2023);
 
 	}
 	outfile.close();
